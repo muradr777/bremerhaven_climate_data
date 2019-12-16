8c0abedcf8e0c582d08faa681a2bc7c0d1155f18
Murad <muradrustamov@gmail.com>
2019-12-16 23:31:26 +0100

    Footer menu

--- webcontent/cgi-bin/data/content.sh
+++ webcontent/cgi-bin/data/content.sh
@@ -3,4 +3,4 @@ team_name="Team 8"
 main_subtitle="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem minus nulla, aut est accusamus aliquid, omnis vero aspernatur quibusdam nihil minima exercitationem repellat molestias sit natus reprehenderit delectus odit amet."
 footer_title="Team 8 - Web Application"
 footer_subtitle="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam placeat, natus quidem possimus sunt fuga adipisci deleniti vitae. Ipsam, est."
-
+footer_year="2020 | Wetter Projekt Aufgabe"
--- webcontent/cgi-bin/footer.sh
+++ webcontent/cgi-bin/footer.sh
@@ -25,29 +25,27 @@ echo '
 					<h5 class="footer-title">Links</h5>
 					<div class="row">
 						<div class="col-6">
-							<ul class="list-unstyled footer-menu">
-								<li><a href="index.html">Home</a></li>
-								<li><a href="categories.html">Data</a></li>
-								<li><a href="#">Docs</a></li>
-								<li><a href="team.html">Team</a></li>
-								<li><a href="credits.html">Credits</a></li>
-							</ul>
+							<ul class="list-unstyled footer-menu"> '
+							cat data/navbar_menu.dat | while read link title; do
+								echo "	<li><a href=\"$link\">$title</a></li>"
+							done
+echo '						</ul>
 						</div>
 						<div class="col-6">
-							<ul class="list-unstyled footer-menu">
-								<li><a href="index.html">Temperatur</a></li>
-								<li><a href="categories.html">Niederschlag</a></li>
-								<li><a href="#">Windstärke</a></li>
-							</ul>
+							<ul class="list-unstyled footer-menu"> '
+							cat data/categories.dat | while read link title icon; do
+								echo "	<li><a href=\"$link\">$title</a></li>"
+							done
+echo '						</ul>
 						</div>
 					</div>
 					
 				</div>
 			</div>
 		</div>
-		<div class="container-fluid text-center page-footer__rights py-2">
-			<small>2020 © Team 8</small>
-		</div>
+		<div class="container-fluid text-center page-footer__rights py-2"> '
+echo "		<small>$footer_year <br/>© $team_name</small>"
+echo '	</div>
 	</footer>
 
 '
