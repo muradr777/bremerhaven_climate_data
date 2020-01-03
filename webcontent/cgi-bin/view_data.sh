41b22bbcceb10f5009d86e92c26e2f8ef8e074e9
Murad <muradrustamov@gmail.com>
2020-01-04 00:22:04 +0100

    Table getting data

--- webcontent/cgi-bin/view_data.sh
+++ webcontent/cgi-bin/view_data.sh
@@ -10,13 +10,11 @@ set -- $QUERY_STRING
 cat_id=$(($2 + 0)) 
 IFS=$SAVE_IFS
 
-i=0
 cat_title=""
 
+i=0
 while read c_link c_title c_icon; do
-	echo "catid= $cat_id i= $i c_title= $c_title <br>"
 	if [ $i = $cat_id ]; then
-		echo "YEEAAAH"
 		cat_title="$c_title"
 	fi
 	((i+=1))
@@ -24,7 +22,7 @@ done <<< $(cat data/categories.dat)
 
 echo '
 	<main class="wrapper container-fluid">
-		<h1>Data Presentation</h1>
+		<h1>${cat_title}daten Presentation</h1>
 		
 		<section class="d_diagramm">
 			<img width="100%" src="../img/plot_test.svg" alt="Plot test" />
