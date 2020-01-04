a9d6e2c8e0df728058c934cf565e8d448db3bf83
Murad <muradrustamov@gmail.com>
2020-01-05 00:02:37 +0100

    Get certain data count

--- webcontent/cgi-bin/get_rain_form1.sh
+++ webcontent/cgi-bin/get_rain_form1.sh
@@ -2,16 +2,16 @@ type=$(($1 + 0))
 
 case $type in 
 	0)
-		echo "Kein Niederschlag (oder kein Wert)"
+		echo "<span class=\"rain_form_block\"><i class=\"fa fa-sun-o text-warning\" aria-hidden=\"true\"></i><span>Kein Niederschlag (oder kein Wert)</span></span>"
 		;;
 	1)
-		echo "nur Regen"
+		echo "<span class=\"rain_form_block\"><i class=\"fa fa-tint text-primary\" aria-hidden=\"true\"></i><span>nur Regen</span></span>"
 		;;
 	2)
-		echo "nur Schnee"
+		echo "<span class=\"rain_form_block\"><i class=\"fa fa-snowflake-o text-primary\" aria-hidden=\"true\"></i><span>nur Schnee</span></span>"
 		;;
 	3)
-		echo "Regen und Schnee (und/oder Schneeregen)"
+		echo "<span class=\"rain_form_block\"><i class=\"fa fa-tint text-primary\" aria-hidden=\"true\"></i><i class=\"fa fa-snowflake-o text-primary\" aria-hidden=\"true\"></i><span>Regen und Schnee (und/oder Schneeregen)</span></span>"
 		;;
 esac
 
