b18b2d1e34bdd71616e9004f8f9d4f45a2901558
mozzieongitt <janmelone@protonmail.com>
2020-01-07 15:39:03 +0100

    user for download; tmp_file deletion

---
+++ clear_tmp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+if [ "$USER" = "step2019team08" ]; then
+  ssh mydocker "echo 'rm -rf /tmp/www-data_*' > /usr/lib/cgi-bin/clear_all.sh ; curl https://informatik.hs-bremerhaven.de/docker$(id -u)/cgi-bin/clear_all.sh ; rm /usr/lib/cgi-bin/clear_all.sh"
+else
+  sudo -u step2019team08 ./clear_tmp.sh
+fi
