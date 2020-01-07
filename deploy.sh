5260d30f4d3b8f46ccccd55b6d872a5ace07e2fd
mozzieongitt <janmelone@protonmail.com>
2020-01-07 16:10:06 +0100

    more changes to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-read branch
+if [ "$1" != "" ]; then branch=$1; 
+else
+  echo "Please input branch name:"
+  read branch
+fi
+
 git pull origin "$branch" 
 cp -r webcontent/cgi-bin/* /usr/lib/cgi-bin/
 cp -r webcontent/html/* /var/www/html/docker11257/
