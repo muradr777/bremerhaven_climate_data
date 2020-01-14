ef060c80eb99879d1b4dec98355c9b468ea3bc65
mozzieongitt <janmelone@protonmail.com>
2020-01-14 17:19:50 +0100

    clear_tmp nun auch vom docker aus

--- clear_tmp.sh
+++ clear_tmp.sh
@@ -12,6 +12,15 @@ if [ "$USER" = "step2019team08" ]; then
   curl https://informatik.hs-bremerhaven.de/docker$(id -u)/cgi-bin/clear_all.sh
   ssh mydocker rm /usr/lib/cgi-bin/clear_all.sh
   rm -rf tmp
-else
+elif [ "$HOSTNAME" == "hopper" ]; then
   sudo -u step2019team08 ./clear_tmp.sh
+elif [ "$HOSTNAME" == "docker11257" ]; then
+  echo '#!/bin/bash
+  rm -rf /tmp/www-data_*
+  echo "content-type: text/html"
+  echo ""
+  ' > /usr/lib/cgi-bin/clear_all.sh
+  chmod +x /usr/lib/cgi-bin/clear_all.sh
+  curl https://informatik.hs-bremerhaven.de/docker11257/cgi-bin/clear_all.sh
+  rm /usr/lib/cgi-bin/clear_all.sh
 fi
