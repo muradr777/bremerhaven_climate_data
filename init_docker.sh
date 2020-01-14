b6dfabf32d5b5105897c2c60ca39dd7c6959a188
mozzieongitt <janmelone@protonmail.com>
2020-01-14 17:21:35 +0100

    init_docker changed

--- init_docker.sh
+++ init_docker.sh
@@ -4,11 +4,16 @@
 # prepares /var/www/html/dockerxxxxx/img/
 
 
-./clear_tmp.sh
 if [ "$USER" == "step2019team08" ];then
+  ./clear_tmp.sh
   ssh mydocker "cd git/step2019team08-WebProjekt ; ./deploy.sh master"
+  ./prepare_img.sh
+elif [ "$HOSTNAME" == "docker11257" ];then
+  ./clear_tmp.sh
+  cd ~/git/step2019team08-WebProjekt; 
+  ./deploy.sh master
+  ./prepare_img.sh
 else
-  echo "not the right user"
+  echo "not the right user or host"
   exit 1
 fi
-./prepare_img.sh
