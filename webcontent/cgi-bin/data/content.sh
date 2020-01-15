94a828b3b89ac0beb60f057aa1e0aabc9bc03905
mozzieongitt <janmelone@protonmail.com>
2020-01-15 21:49:02 +0100

    Typo

--- webcontent/cgi-bin/data/content.sh
+++ webcontent/cgi-bin/data/content.sh
@@ -1,6 +1,6 @@
 main_title="Hallo von"
 team_name="Team 8"
-main_subtitle="In diesem Projekt sollen wir mit Hilfe der Daten vom Deutschen Wetterdienst eine dynamische Webanwendung zur Darstellung von klimarelevanten Daten implementieren. Für einen Teil des Projekts soll ein UML-Diagramm, wie in SWE1 gelernt, erstellt werden. Mit Hilfe der verschiedenen Technologien wie HTML, CGI, BASH, SVG usw. werden wir sowohl die Skripte, Tabellen und Diagramme erstellen als auch die Webseite darstellen. Die Webseite wird in Module getrennet sein und dadurch im front-end leich erweiterbar sein. "
+main_subtitle="In diesem Projekt sollen wir mit Hilfe der Daten vom Deutschen Wetterdienst eine dynamische Webanwendung zur Darstellung von klimarelevanten Daten implementieren. Für einen Teil des Projekts soll ein UML-Diagramm, wie in SWE1 gelernt, erstellt werden. Mit Hilfe der verschiedenen Technologien wie HTML, CGI, BASH, SVG usw. werden wir sowohl die Skripte, Tabellen und Diagramme erstellen als auch die Webseite darstellen. Die Webseite wird in Module getrennt sein und dadurch im front-end leicht erweiterbar sein. "
 footer_title="Team 8 - Web Application"
 footer_subtitle="$main_subtitle"
 footer_year="2020 | Wetter Projekt Aufgabe"
