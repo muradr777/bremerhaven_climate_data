3e7644aa4ec4d9d671ac6d29fea2951683cdff01
mozzieongitt <janmelone@protonmail.com>
2020-01-17 09:31:52 +0100

    added poster

--- documentation/poster/mkposter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pdflatex poster.tex
-bibtex poster.aux
-pdflatex poster.tex
-pdflatex poster.tex
-
-#ls -1rt | tail -n+6 | grep -v pdf | grep -v tex | xargs rm
-rm poster.aux poster.bbl poster.blg poster.log
